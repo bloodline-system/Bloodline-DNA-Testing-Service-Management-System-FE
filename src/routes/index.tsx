ddc24012d9bf60cab29e34eb2242cc4d471a4c95
Thuytien4325 <thuyytien4325@gmail.com>
2026-04-16 04:17:08 +0700

    UI report,order

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
 import HomePage from "@/pages/HomePage";
 import SignInPage from "@/pages/SignInPage";
 import SignUpPage from "@/pages/SignUpPage";
+import OrderPage from "@/pages/OrderPage";
+import ReportPage from "@/pages/ReportPage";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 const AppRoutes = () => {
@@ -11,6 +13,8 @@ const AppRoutes = () => {
           <Route index element={<HomePage />} />
           <Route path="/sign-in" element={<SignInPage />} />
           <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/order" element={<OrderPage />} />
+          <Route path="/report" element={<ReportPage />} />
         </Routes>
       </BrowserRouter>
     </>
